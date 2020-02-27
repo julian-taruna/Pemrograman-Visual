f4f8a5b0d8fd7a4ca14e1f5798ea8647b561e591
julian-taruna <57337707+julian-taruna@users.noreply.github.com>
2020-02-27 14:30:10 +0700

    Add files via upload
    Pemrograman Visual-ElectronPart1

---
+++ index.js
@@ -0,0 +1,23 @@
+const electron = require("electron");
+
+const {app, BrowserWindow, MEnu, ipcMain} = electron ;
+
+let todayWindow;
+let creteWindow;
+let listWindow;
+
+app.on("ready", ()=> {
+    todayWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        title : "Aplikasi Dokter"
+    });
+
+    todayWindow.loadURL(`file://${__dirname}/today.html`);
+    todayWindow.on("closed", ()=> {
+
+        app.quit();
+        todayWindow = null;
+    })
+});
