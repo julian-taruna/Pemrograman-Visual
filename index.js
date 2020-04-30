dad3cbf92ea01e404f07a400064a850fde996ce4
julian-taruna <57337707+julian-taruna@users.noreply.github.com>
2020-04-30 13:03:24 +0700

    Trial video part-10
    create local database in PC

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const electron = require("electron");
 const uuid = require("uuid");
+const fs = require('fs')
 const {
     app, 
     BrowserWindow, 
@@ -13,6 +14,13 @@ let listWindow;
 
 let allAppointment = [];
 
+fs.readFile("db.json", (err, jsonAppointment) =>{
+    if(!err){
+        const oldAppointment = JSON.parse(jsonAppointment);
+        allAppointment = oldAppointment;
+    }
+})
+
 app.on("ready", ()=> {
     todayWindow = new BrowserWindow({
         webPreferences: {
@@ -23,6 +31,11 @@ app.on("ready", ()=> {
 
     todayWindow.loadURL(`file://${__dirname}/today.html`);
     todayWindow.on("closed", ()=> {
+
+        const jsonAppointment = JSON.stringify(allAppointment);
+        fs.writeFileSync("db.json", jsonAppointment);
+
+
 
         app.quit();
         todayWindow = null;
